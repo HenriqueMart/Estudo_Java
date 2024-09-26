3f01172986c5a914b5e26904007b4772a7c87658
DevMartins <h.martins.silva2014@gmail.com>
2024-09-26 18:52:28 -0300

    docs: Realizando anotações de Operadores

--- java-operadores/README.md
+++ java-operadores/README.md
@@ -3,3 +3,21 @@
 Char vai usar aspa Simples
 
 Caso a operação tenha um caractere entre elas, ele vai concatenar todos os elementos +, Mas caso os números tenha evidência ele primeira realizar a evidência
+
+Anoto os termos pois não conhecia o nome dos tipos utilizados
+Operador Unário +,-,!
+
+Tornar o valor positivo
+
+var * -1;
+
+numero ++ -> Não atualizado no momento
+++ numero -> Atualiado no momento
+
+Condição Ternário ?:
+
+Relacional >=, <= ...
+
+Comparando de Objeto utilizamos o 
+
+´´var.equals()´´
--- java-operadores/src/App.java
+++ java-operadores/src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        //Convertendo em positivo
+        numero = numero * 1;
+
+        //Condição Ternária 
+        int a = 5, b = 7;
+
+        String resultado =  a==b ? "Verdadeiro":"Falso";
+
+        System.out.println(resultado);
+
+        //Comparando String
+
+        String nome = "Jose", nome2 = "Jose";
+
+        System.out.println(nome == nome2);
+
+        //Forma para compara só o elemento dentro do objeto
+        System.out.println(nome.equals(nome2));
+
     }
 }
