d79d62230a41ffd9d2e171489ba8761dea8b5cc4
DevMartins <h.martins.silva2014@gmail.com>
2024-10-03 18:38:07 -0300

    feat - for com array

---
+++ java-estruturacondicionais/src/ExemploForArray.java
@@ -0,0 +1,9 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos [] = {"Felipe", "Jonas", "Julio", "Marcos"};
+
+        for (String aluno : alunos) {
+            System.out.println("O aluno no indice x=  é " + aluno);
+        }
+    }
+}
