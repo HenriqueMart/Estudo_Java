7297318b77add5a6db85c01f55a3f6363b6f3874
DevMartins <h.martins.silva2014@gmail.com>
2024-10-12 01:03:09 -0300

    fead - Novo desafio Linha telefônica

---
+++ Execicios/DesafiaControleDeFluxo/src/DesafioLinhaTelefonica.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DesafioLinhaTelefonica {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Entrada do serviço a ser verificado
+        String servico = scanner.nextLine().trim();
+        
+        // Entrada do nome do cliente e os serviços contratados
+        String entradaCliente = scanner.nextLine().trim();
+        
+        // Separando o nome do cliente e os serviços contratados
+        String[] partes = entradaCliente.split(",");
+        String nomeCliente = partes[0];
+        boolean contratado = false;
+        
+        // TODO: Verifique se o serviço está na lista de serviços contratados
+        for (int i = 1; i < partes.length; i++) {
+            if(servico.equals(partes[i].trim())){
+                contratado = true;
+            }
+        }
+        
+          
+          if(contratado){
+            System.out.println("Sim");
+          }else{
+            System.out.println("Nao");
+          }
+    
+        scanner.close();
+  }
+}
