6033b5e6490a56b488d10ca3698b4a5138703d81
DevMartins <h.martins.silva2014@gmail.com>
2024-10-18 20:16:21 -0300

    FEAT - Utilizando Enums em Java

---
+++ java_Enums/src/EstadoBrasileiro.java
@@ -0,0 +1,30 @@
+
+public enum EstadoBrasileiro {
+	SAO_PAULO("SP", "São Paulo"),
+	RIO_JANEIRO("RJ","Rio de Janeiro"),
+	PIUAI("PI", "Piauí"),
+	MARANHAO("MA","Maranhão"),
+	CEARA("CE", "Ceará");
+	
+	private String nome;
+	private String sigla;
+	
+	private EstadoBrasileiro(String sigla, String nome) {
+		this.sigla = sigla;
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public String getNomeMaiusculo() {
+		return nome.toUpperCase();
+	}
+	
+	
+}
